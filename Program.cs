111a4436404c9016a750d3d1d9009f6d4bae1c12
Vobluda <simon2022@standrews.ac.th>
2023-06-17 11:36:50 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+
+class Program {
+    
+
+    public static void Main() {
+        WriteLine("Hello World");
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Šimon Libřický
+
+Charles University Programming II Semester Project
+
+Ultimate Tic Tac Toe Minimax Player
